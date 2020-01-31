333716599516292479da5385645871a165e9c584
PeterArriaza <peter.arriaza@gmail.com>
2020-01-31 12:10:36 -0500

    updated api get catch all url

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 const PORT = process.env.PORT || 8080;
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
 app.get("/*", (req, res) => {
   res.json({ ok: true });
