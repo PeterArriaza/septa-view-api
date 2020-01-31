dc62c575351e5ac1aa436d25d894e0ece746a93b
PeterArriaza <peter.arriaza@gmail.com>
2020-01-31 07:00:45 -0500

    updated api get catch all url

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.get("/api/*", (req, res) => {
+app.get("/*", (req, res) => {
   res.json({ ok: true });
 });
 
